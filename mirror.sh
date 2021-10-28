696b51989a20b575845d38e034a758b08412561e
Michael C <michael@mchang.name>
2021-10-28 19:54:25 -0400

    validation

--- mirror.sh
+++ mirror.sh
@@ -4,22 +4,33 @@ cd /var/www/sponsorblock || exit
 function download {
   URL=sponsor.ajay.app
   # download from main server so get filenames
-  curl -s -L $URL/database.json?generate=false -o response.json
-  DUMP_DATE=$(cat response.json | jq .lastUpdated)
-  tables=$(cat response.json | jq -r .links[].table)
+  curl -sL $URL/database.json?generate=false -o response.json
+  EXTRAPARAM=$@
+  DUMP_DATE=$(jq .lastUpdated < response.json)
+  tables=$(jq -r .links[].table < response.json)
   # set $@ since posix doesn't have named variables
   for table in $tables
   do
-    rsync -ztvP --zc=lz4 --append rsync://$URL/sponsorblock/${table}_${DUMP_DATE}.csv latest/${table}.csv
+    rsync -aztvP --zc=lz4 "$EXTRAPARAM" --append rsync://$URL/sponsorblock/"${table}"_"${DUMP_DATE}".csv latest/"${table}".csv
   done
   rm response.json
-  echo $DUMP_DATE > lastUpdate.txt
+  echo "$DUMP_DATE" > latest/lastUpdate.txt
 }
 
 function validate {
-  for file in latest/*.csv
-  do csvlint $file && echo $file is valid || rm $file; done
-  download
+  FAIL=0
+  for file in latest/*.csv; do
+    if ! csvlint "$file"; then
+      rm "$file"
+      FAIL=1
+    fi
+  done
+  echo $FAIL
+  if [ $FAIL -eq 1 ]; then
+    FAIL=0
+    echo "Downloading failed files"
+    download --ignore-existing
+  fi
 }
 
 download
